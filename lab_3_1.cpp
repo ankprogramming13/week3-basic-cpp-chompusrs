b3c38f7d2941d820e202b0184a949d7977a8eb75
Unknown <sarisa.050241@gmail.com>
2017-11-21 17:37:25 +0700

    ข้อที่ 1

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,5 +4,11 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
+  cout << "Ms. sarisa onprasert" << endl;
+  cout << "chompu" << endl;
+  cout << "female" << endl;
+  cout << "05/02/1197" << endl;
+  cout << "swim" << endl;
+  cout << "single" << endl;
   return 0;
 }
